7c0b280c623a7257b2952b7d5bbdb558767c01f8
Cameron Montgomery <66389466+Tribalash@users.noreply.github.com>
2022-01-24 11:17:50 -0700

    Started work on templates; index view created and tested; CSS file moved to public/css; break nav-bar and footer to partials

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,29 @@ const express = require('express')
 const hbs = require('hbs')
 require('./db/mongoose')
 
-const userRouter = require('./routes/userRouter')
-
+//const userRouter = require('./routes/userRouter')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Express configuration paths
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+
+// config static directory
+app.use(express.static(publicDirectoryPath))
 
 app.use(express.json())
-app.use(userRouter)
+//app.use(userRouter)
 
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`)
