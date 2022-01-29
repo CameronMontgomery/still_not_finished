403812f48ce8ff5193089a4e5069ccd76264c5aa
Cameron Montgomery <66389466+Tribalash@users.noreply.github.com>
2022-01-28 18:48:13 -0700

    Created update user route and logic to make sure updates are allowed; added bcryptjs npm package

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,42 +1,63 @@
-const express = require('express');
-const User = require('../models/user');
+const express = require("express");
+const User = require("../models/user");
 const router = new express.Router();
 
-router.post('/users', async (req, res) => {
-  const user = new User(req.body)
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
 
   try {
-    await user.save()
-    res.status(201).render('signIn')
+    await user.save();
+    res.status(201).render("signIn");
   } catch (e) {
-    res.status(400).send(e)
+    res.status(400).send(e);
   }
-  
-})
+});
 
-router.get('/users', async (req, res) => {
+router.get("/users", async (req, res) => {
   try {
-    const users = await User.find({})
-    res.send(users)
+    const users = await User.find({});
+    res.send(users);
   } catch (e) {
-    res.status(404).send()
+    res.status(404).send();
   }
-})
+});
 
-router.get('/users/:id', async (req, res) => {
+router.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
 
   try {
-    const user = await User.findById(_id)
+    const user = await User.findById(_id);
 
     if (!user) {
-      return res.status(404).send()
+      return res.status(404).send();
     }
 
-    res.send(user)
+    res.send(user);
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send(e);
   }
-})
+});
 
-module.exports = router
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["firstName", "lastName", "email", "password"];
+  const isValid = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValid) {
+    return res.status(400).send("Invalid Update Information");
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
