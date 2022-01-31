9fcbc1c621a5b876a71e2063ce6337629b8f9661
Cameron Montgomery <66389466+Tribalash@users.noreply.github.com>
2022-01-31 11:04:37 -0700

    Created user.findByCredential method to look up user by email and compare supplied password with the stored hashed password; created post method route for user/signIn

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -15,6 +16,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
@@ -34,6 +36,30 @@ const userSchema = new mongoose.Schema({
       }
     }
   }
+})
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = User.findOne({ email })
+
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('Unable to login, email or password doesn\'t match')
+  }
+
+  return user
+}
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8)
+  }
+
+  next()
 })
 
 const User = mongoose.model('User', userSchema)
